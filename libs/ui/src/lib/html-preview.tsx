b436be8ff9eb51f1e8e36a5d21149c4d7a5b3a51
Mayfield <3902405+mayfieldiv@users.noreply.github.com>
2021-02-19 20:48:23 -0500

    html preview working

--- libs/ui/src/lib/html-preview.tsx
+++ libs/ui/src/lib/html-preview.tsx
@@ -1,20 +1,61 @@
 import React from 'react';
-
 import styled from 'styled-components';
 
-/* eslint-disable-next-line */
-export interface HtmlPreviewProps {}
+export interface HtmlPreviewProps {
+  htmlSource: string;
+  width: string;
+  height: string;
+}
 
-const StyledHtmlPreview = styled.div`
-  color: pink;
+const Container = styled.div`
+  padding: 1.25rem;
+  display: flex;
+  justify-content: center;
 `;
 
-export function HtmlPreview(props: HtmlPreviewProps) {
-  return (
-    <StyledHtmlPreview>
-      <h1>Welcome to html-preview!</h1>
-    </StyledHtmlPreview>
-  );
+const Preview = styled.iframe`
+  background: white;
+  border: 0;
+  outline: 0;
+`;
+
+export class HtmlPreview extends React.PureComponent<HtmlPreviewProps> {
+  private frameRef: React.RefObject<HTMLIFrameElement>;
+
+  constructor(props: HtmlPreviewProps) {
+    super(props);
+    this.frameRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <Container>
+        <Preview
+          ref={this.frameRef}
+          // allow-same-origin to access contentDocument
+          sandbox="allow-same-origin"
+          height={this.props.height}
+          width={this.props.width}
+        ></Preview>
+      </Container>
+    );
+  }
+
+  componentDidMount() {
+    this.updateFrameContent();
+  }
+
+  componentDidUpdate() {
+    this.updateFrameContent();
+  }
+
+  private updateFrameContent() {
+    if (this.frameRef.current?.contentDocument?.body != null) {
+      this.frameRef.current.contentDocument.head.innerHTML =
+        '<style>body{overflow:hidden}</style>';
+      this.frameRef.current.contentDocument.body.innerHTML = this.props.htmlSource;
+    }
+  }
 }
 
 export default HtmlPreview;
